9f519ddf3f7529a35c71060c95ab4cf55a2809ec
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-28 19:17:35 +0100

    Removed the unneeded file

--- project_name/webpack.config.js
+++ project_name/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,9 +12,21 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.css'],
   },
+  // optimization: {
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       exclude: /\/node_modules/,
+  //     }),
+  //   ],
+  // },
   plugins: [
     // add plugins here
   ],
+  // performance: {
+  //   hints: false,
+  //   maxEntrypointSize: 512000,
+  //   maxAssetSize: 512000,
+  // },
   devServer: {
     publicPath: path.resolve(__dirname, '/bundle/'),
     historyApiFallback: true,
