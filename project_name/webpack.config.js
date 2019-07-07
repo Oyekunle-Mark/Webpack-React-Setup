f15c49d947e05edad4700dfda2dfade062cec47d
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-07 17:28:21 +0100

    Uncommented production settings and used the webpack clean library

--- project_name/webpack.config.js
+++ project_name/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,21 +13,21 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.css'],
   },
-  // optimization: {
-  //   minimizer: [
-  //     new TerserPlugin({
-  //       exclude: /\/node_modules/,
-  //     }),
-  //   ],
-  // },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        exclude: /\/node_modules/,
+      }),
+    ],
+  },
   plugins: [
-    // add plugins here
+    new CleanWebpackPlugin(),
   ],
-  // performance: {
-  //   hints: false,
-  //   maxEntrypointSize: 512000,
-  //   maxAssetSize: 512000,
-  // },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   devServer: {
     publicPath: path.resolve(__dirname, '/bundle/'),
     historyApiFallback: true,
