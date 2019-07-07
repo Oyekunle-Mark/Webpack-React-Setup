c40d570e01a79fbc7306e6ad993cb92b1e5c36d7
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-07 19:05:17 +0100

    Changed the default message

--- project_name/src/App.jsx
+++ project_name/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import SampleComponent from './components/SampleComponent';
+
 const App = () => (
   <div>
-    <h1>Hello, World!</h1>
+    <h1>I am the container</h1>
+    <SampleComponent />
   </div>
 );
 
