7682a94aaae57972c083f6b2fcee13dc3c717577
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-07 19:01:10 +0100

    The webpack development configurations

---
+++ project_name/webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    publicPath: path.resolve(__dirname, '/bundle/'),
+    historyApiFallback: true,
+    compress: true,
+    open: true,
+    hot: true,
+    port: 9000,
+  },
+  plugins: [new HardSourceWebpackPlugin()],
+});
